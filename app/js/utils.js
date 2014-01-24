64c986b852eba0c9fb29f0429b22a01bd6475d09
bcandullo <brad.candullo.contractor@macmillan.com>
2014-01-24 13:26:47 -0500

    css fixes, project button animation

--- app/js/utils.js
+++ app/js/utils.js
@@ -1,5 +1,23 @@
 window.B.utils = (function () {
 
+	/*
+	* support 1st gen ipad (function.prototype.bind)
+	* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
+	*/
+	if (!Function.prototype.bind) {
+		Function.prototype.bind = function (oThis) {
+			var aArgs = Array.prototype.slice.call(arguments, 1), 
+				fToBind = this, 
+				fNOP = function () {},
+				fBound = function () {
+					return fToBind.apply(this instanceof Function && oThis ? this : oThis, aArgs.concat(Array.prototype.slice.call(arguments)));
+				};
+			fNOP.prototype = this.prototype;
+			fBound.prototype = new fNOP();
+			return fBound;
+		};
+	}
+
 	window.$ = document.querySelector.bind(document);
 	window.$$ = document.querySelectorAll.bind(document);
 
@@ -78,21 +96,3 @@ window.B.utils = (function () {
 
 
 }());
-
-/*
-* support 1st gen ipad (function.prototype.bind)
-* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
-*/
-if (!Function.prototype.bind) {
-	Function.prototype.bind = function (oThis) {
-		var aArgs = Array.prototype.slice.call(arguments, 1), 
-			fToBind = this, 
-			fNOP = function () {},
-			fBound = function () {
-				return fToBind.apply(this instanceof Function && oThis ? this : oThis, aArgs.concat(Array.prototype.slice.call(arguments)));
-			};
-		fNOP.prototype = this.prototype;
-		fBound.prototype = new fNOP();
-		return fBound;
-	};
-}
